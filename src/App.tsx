7df535817f0ec92d6914b26a0d448f6bff9be935
Benji <benjaminpeto23@gmail.com>
2022-12-04 15:50:38 +0000

    initialized project with vite, react, ts, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+      Install and Setup Vite with React + Typescript + Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
